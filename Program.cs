7837a4cf84b6477bd01df509b72f9c208d5477a1
Petr Starostin <starostin.petr@yandex.ru>
2023-03-08 15:35:03 +0400

    решение второй задачи

--- Program.cs
+++ Program.cs
@@ -8,3 +8,29 @@
 //     Console.WriteLine($"Число {num2} больше {num1}");
 // }
 // else Console.WriteLine($"Число {num1} больше {num2}");
+
+//Вторая задача
+Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if(num1 < num2)
+{
+    if(num2<num3) 
+    {
+         Console.WriteLine($"Число {num3} максимальное");
+    }
+    else        
+        Console.WriteLine($"Число {num2} максимальное");    
+}
+else
+{
+    if(num1<num3)
+    { 
+        Console.WriteLine($"Число {num3} максимальное");
+    } 
+    else
+        Console.WriteLine($"Число {num1} максимальное");    
+}
